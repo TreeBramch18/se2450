9a3e5ee4b57762f4e400bc3ccee66da020ab381a
TreeBramch18 <bannanaface18@gmail.com>
2025-08-27 10:37:49 -0600

    created Age guesser

---
+++ AgeGuesser.py
@@ -0,0 +1,11 @@
+import random
+
+print("Hello there, I'm going to try and guess your age")
+name = input("What's your name?")
+while True:
+    ageGuess = random.randint(15,30)
+    result = input(f"Are you {ageGuess} years old? Y or N?")
+    if result in "Y y":
+        print(f"Yay, {name} is {ageGuess} years old")
+        break
+    print("Rats")
