ef23f127b774dbdc097d0d29abf512f5b17a0e21
TreeBramch18 <bannanaface18@gmail.com>
2025-08-27 10:45:33 -0600

    Added Comments

--- AgeGuesser.py
+++ AgeGuesser.py
@@ -2,8 +2,12 @@ import random
 
 print("Hello there, I'm going to try and guess your age")
 name = input("What's your name?")
+
+# Runs a random age guess until correct age reached
 while True: 
-    ageGuess = random.randint(15,30)
+    MinAge = 15
+    MaxAge = 30
+    ageGuess = random.randint(MinAge,MaxAge)
     result = input(f"Are you {ageGuess} years old? Y or N?")
     if result in "Y y":
         print(f"Yay, {name} is {ageGuess} years old")
